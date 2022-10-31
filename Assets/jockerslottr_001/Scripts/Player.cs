c12c54dc68a2b48963916b1de33e2195104b8d41
Sladkiy Eduard <smallgamescompany@gmail.com>
2022-10-31 12:27:11 +0300

    update

--- Assets/jockerslottr_001/Scripts/Player.cs
+++ Assets/jockerslottr_001/Scripts/Player.cs
@@ -4,4 +4,40 @@ public class Player : MonoBehaviour
 {
     [SerializeField] Transform cardPlace;
     [SerializeField] Transform cardHand;
+
+    [SerializeField] float radius;
+
+    private void Start()
+    {
+        UpdateCards();
+    }
+
+    public void UpdateCards()
+    {
+        for(int i = 0; i < cardHand.childCount; i++)
+        {
+            cardHand.GetChild(i).RotateAround(cardHand.position, Vector3.forward, i * 6);
+        }
+
+        //float step = 1;
+        //for(int i = 0; i < cardHand.childCount; i++, step -= 10)
+        //{
+        //    /* Distance around the circle */
+        //    var radians = 2 * Mathf.PI / step;
+
+        //    /* Get the vector direction */
+        //    var vertical = Mathf.Sin(radians);
+        //    var horizontal = Mathf.Cos(radians);
+
+        //    var spawnDir = new Vector3(horizontal, vertical, 0);
+
+        //    /* Get the spawn position */
+        //    var spawnPos = cardHand.position + spawnDir * radius; // Radius is just the distance away from the point
+
+        //    cardHand.GetChild(i).transform.position = spawnPos;
+
+        //    /* Rotate the enemy to face towards player */
+        //    //cardHand.GetChild(i).transform.LookAt(cardHand.position, Vector2.up);
+        //}
+    }
 }
