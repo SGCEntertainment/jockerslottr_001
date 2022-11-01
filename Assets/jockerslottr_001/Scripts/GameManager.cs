bd4636e53762299c72812d458876c48e7970185b
Sladkiy Eduard <smallgamescompany@gmail.com>
2022-11-01 13:16:11 +0300

    update

--- Assets/jockerslottr_001/Scripts/GameManager.cs
+++ Assets/jockerslottr_001/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ public class GameManager : MonoBehaviour
 {
     const int maxCards = 8;
     [SerializeField] CardDeckData cardDeckData;
+    [SerializeField] Transform lookAtPoint;
 
     [Space(10)]
     [SerializeField] Player[] players;
@@ -18,16 +19,22 @@ public class GameManager : MonoBehaviour
 
     IEnumerator Deal—ards()
     {
-        float timeOffset = 0.1f;
+        float timeOffset = 0;
         foreach (Player p in players)
         {
             for(int i = 0; i < maxCards; i++)
             {
-                //Instantiate(cardDeckData.cardPrefab, p.cardHand.position, Quaternion.identity,  p.cardHand);
+                Instantiate(cardDeckData.cardPrefab, p.cardHand.position, Quaternion.identity,  p.cardHand);
                 p.UpdateCards();
 
                 yield return new WaitForSeconds(timeOffset);
             }
+
+            Vector3 diff = lookAtPoint.position - p.cardHand.position;
+            diff.Normalize();
+
+            float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+            p.cardHand.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
         }
     }
 }
--- Assets/jockerslottr_001/Scripts/Player.cs
+++ Assets/jockerslottr_001/Scripts/Player.cs
@@ -7,9 +7,38 @@ public class Player : MonoBehaviour
 
     public void UpdateCards()
     {
+        //for (int i = 0; i < cardHand.childCount; i++)
+        //{
+        //    cardHand.GetChild(i).RotateAround(cardHand.position, Vector3.forward, i);
+        //}
+
+        Vector3 center = transform.position;
         for (int i = 0; i < cardHand.childCount; i++)
         {
-            cardHand.GetChild(i).RotateAround(cardHand.position, Vector3.forward, i);
+            float angle = i switch
+            {
+                0 => -40,
+                1 => -30,
+                2 => -20,
+                3 => -10,
+                4 => 0,
+                5 => 10,
+                6 => 20,
+                7 => 30,
+            };
+
+            Vector3 pos = RandomCircle(center, 1, angle);
+            Quaternion rot = Quaternion.FromToRotation(Vector2.up, center - pos);
+            cardHand.GetChild(i).SetPositionAndRotation(pos, rot);
         }
+    }
+
+    Vector3 RandomCircle(Vector3 center, float radius, float angle)
+    {
+        Vector3 pos;
+        pos.x = center.x + radius * Mathf.Sin(angle * Mathf.Deg2Rad);
+        pos.y = center.y + radius * Mathf.Cos(angle * Mathf.Deg2Rad);
+        pos.z = center.z;
+        return pos;
     }
 }
